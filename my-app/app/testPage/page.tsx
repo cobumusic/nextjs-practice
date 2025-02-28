8ab4a3b8e2a42dbf968ad85f0a72674bd183bcc4
cobumusic <jacobmugalde@gmail.com>
2025-02-27 18:25:30 -0800

    added a nested test page

--- my-app/app/testPage/page.tsx
+++ my-app/app/testPage/page.tsx
@@ -43,15 +43,15 @@ export default function Home() {
           </a>
           <a
             className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            //href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            href="./testPage"
+            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+            target="_blank"
             rel="noopener noreferrer"
           >
-            Next Page
+            Read our docs
           </a>
         </div>
       </main>
-      {/* <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
+      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
         <a
           className="flex items-center gap-2 hover:underline hover:underline-offset-4"
           href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
@@ -97,7 +97,7 @@ export default function Home() {
           />
           Go to nextjs.org →
         </a>
-      </footer> */}
+      </footer>
     </div>
   );
 }
