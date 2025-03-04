8780c833a3a7459d7f21bb9a5183bce3cb51ecd4
cobumusic <jacobmugalde@gmail.com>
2025-03-03 18:22:23 -0800

    made like buttons functional

--- my-app/app/dump/page.tsx
+++ my-app/app/dump/page.tsx
@@ -3,7 +3,6 @@
 import Image from "next/image";
 import { ReactNode } from "react";
 import { useState } from 'react';
-import LikeButton2 from "../like-button-2";
 
 interface CustomProps {
     clickFunction: any;
@@ -16,18 +15,27 @@ interface TestCompProps {
 }
 
 export default function Home() {
-    const [count, setCount] = useState(0);
+    const [likeCount, setCount] = useState(0);
     const names: string[] = ["Item 1: ", "Item 2: "];
 
     const handleClick = () => {
-        setCount(count + 1);
+        console.log("incrementing");
+        setCount(likeCount + 1);
+    }
+
+    function reset(){
+        console.log("reset");
+        setCount(0);
     }
 
     return (
         <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
         <main className="flex flex-col gap-8 row-start-2 items-center sm:items-center">
             <LikeButton />
-            <LikeButton2 />
+            <div className="flex flex-col bg-gray-400 px-5 py-2">
+                <LikeButton2 onClick={handleClick} count={likeCount} />
+                <ResetButton onClick={reset}>Reset</ResetButton>
+            </div>
             <TestComp string1="Item " string2="0: " />
             <ul>
                 {names.map((name) => (
@@ -41,17 +49,40 @@ export default function Home() {
     );
 }
 
+interface IProps {
+    children?: React.ReactNode | string;
+    onClick: (e: any) => void;
+    count?: number;
+  }
+
+const LikeButton2: React.FC<IProps> = (props) => {
+    return (
+        <button className="text-2xl bg-blue-100 text-gray-800 px-2 hover:bg-blue-300 transition-all max-w-xs rounded-md mb-2" onClick={props.onClick}>
+            Like Counter: {props.count}
+        </button>
+    );
+}
+
+const ResetButton: React.FC<IProps> = (props) => {
+    return (
+        <button className="text-2xl bg-red-100 text-gray-800 px-2 hover:bg-red-300 transition-all rounded-md" onClick={props.onClick}>
+            {props.children}
+        </button>
+    );
+};
+
 function LikeButton(){
     const [likes, setLikes] = useState(0);
 
     function handleClick(){
-        console.log('increment like count');
         setLikes(likes + 1);
     }
 
     return(
         <>
-            <button className="text-2xl bg-blue-100 text-gray-800 px-2 hover:bg-blue-300 transition-all max-w-xs rounded-md" onClick={handleClick}>Likes ({likes})</button>
+            <button className="text-2xl bg-blue-100 text-gray-800 px-2 hover:bg-blue-300 transition-all max-w-xs rounded-md" onClick={handleClick}>
+                Likes ({likes})
+            </button>
         </>
     );
 }
